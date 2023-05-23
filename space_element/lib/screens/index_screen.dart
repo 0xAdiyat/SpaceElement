9f263f21622f5b241f33327b8cc404889d971dfd
Huss4in007 <smdhussainmolla@gmail.com>
2023-05-23 21:21:15 +0600

    Added required files and elements

---
+++ space_element/lib/screens/index_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class IndexScreen extends StatelessWidget {
+  const IndexScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
