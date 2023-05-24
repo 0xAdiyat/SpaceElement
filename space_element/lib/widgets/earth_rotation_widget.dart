0b2f1b760863be60b5eee195cf44232f0b864178
Huss4in007 <smdhussainmolla@gmail.com>
2023-05-24 11:51:58 +0600

    Added bottom part

---
+++ space_element/lib/widgets/earth_rotation_widget.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class EarthRotationWidget extends StatefulWidget {
+  @override
+  _EarthRotationWidgetState createState() => _EarthRotationWidgetState();
+}
+
+class _EarthRotationWidgetState extends State<EarthRotationWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 5),
+      vsync: this,
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RotationTransition(
+      turns: _controller,
+      child: Container(
+        width: 200, // adjust the size as per your requirement
+        height: 200,
+        child: Image.asset(
+          "lib/assets/images/earth.png",
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
